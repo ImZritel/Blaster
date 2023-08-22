febb68eeeb1ef616290a4713d706598e500268ce
Zritel <manin.tech@yandex.ru>
2023-08-22 15:49:44 +0200

    Pickup widget class. OnSphereOverlap.

--- Source/Blaster/Weapon/Weapon.h
+++ Source/Blaster/Weapon/Weapon.h
@@ -28,11 +28,23 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnSphereOverlap(   // callback
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
+
 private:
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties.")
 	USkeletalMeshComponent* WeaponMesh;
 	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties.")
 	class USphereComponent* AreaSphere;
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties.")
 	EWeaponState WeaponState;
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties.")
+	class UWidgetComponent* PickupWidget;
 };
